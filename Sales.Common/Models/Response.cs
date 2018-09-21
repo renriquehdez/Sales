7f09c21b3a3da76aa74823ea6768c21bd56d5f72
renriquehdez <renriquehdez@gmail.com>
2018-09-21 16:24:13 -0600

    Consumo de Servicio Rest Full

---
+++ Sales.Common/Models/Response.cs
@@ -0,0 +1,22 @@
+﻿namespace Sales.Common.Models
+{
+    public class Response
+    {
+        #region Properties
+        public bool IsSuccess
+        {
+            get; set;
+        }
+
+        public string Message
+        {
+            get; set;
+        }
+
+        public object Result
+        {
+            get; set;
+        }
+        #endregion
+    }
+}
