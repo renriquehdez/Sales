6ae1ec577d37a15697b6a9e507077917e7661030
renriquehdez <renriquehdez@gmail.com>
2018-09-21 15:56:33 -0600

    Backend and API Services

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace Sales.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Product
+    {
+        #region Properties
+        [Key]
+        public int ProductID
+        {
+            get; set;
+        }
+
+        [Required]
+        public string Description
+        {
+            get; set;
+        }
+
+        public Decimal Price
+        {
+            get; set;
+        }
+
+        public bool IsAvailable
+        {
+            get; set;
+        }
+
+        public DateTime PublishOn
+        {
+            get; set;
+        } 
+        #endregion
+    }
+}
---
+++ Sales.Domain/Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace Sales.Domain.Models
+{
+    using System.Data.Entity;
+    public class DataContext : DbContext
+    {
+        #region Constructors
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+        #endregion
+
+        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products
+        {
+            get; set;
+        }
+    }
+}
