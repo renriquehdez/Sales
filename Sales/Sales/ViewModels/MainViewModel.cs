d74d2c7fca7b1e5b92398f19957d9cd9191c10dc
renriquehdez <renriquehdez@gmail.com>
2018-10-05 15:43:48 -0600

    Navigation Page

--- Sales/Sales/ViewModels/MainViewModel.cs
+++ Sales/Sales/ViewModels/MainViewModel.cs
@@ -1,5 +1,12 @@
-﻿namespace Sales.ViewModels
+﻿
+
+namespace Sales.ViewModels
 {
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using Sales.Views;
+    using Xamarin.Forms;
+
     public class MainViewModel
     {
         #region Properties
@@ -13,6 +20,21 @@
         public MainViewModel()
         {
             this.Products = new ProductsViewModel();
+        }
+        #endregion
+
+        #region Commands
+        public ICommand AddProductCommand
+        {
+            get
+            {
+                return new RelayCommand(GoToAddProduct);
+            }
+        }
+
+        private async void GoToAddProduct()
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(new AddProductPage());
         }
         #endregion
     }
