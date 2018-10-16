a45b58d1a80ce0ea792e91cb1e1592b8693f1031
renriquehdez <renriquehdez@gmail.com>
2018-10-16 17:07:35 -0600

    SalesAPI

--- Sales.API/Controllers/ProductsController.cs
+++ Sales.API/Controllers/ProductsController.cs
@@ -1,18 +1,15 @@
 ﻿namespace Sales.API.Controllers
 {
     using System;
-    using System.Collections.Generic;
-    using System.Data;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
     using System.Linq;
     using System.Net;
-    using System.Net.Http;
     using System.Threading.Tasks;
     using System.Web.Http;
     using System.Web.Http.Description;
-    using Sales.Common.Models;
-    using Sales.Domain.Models;
+    using Common.Models;
+    using Domain.Models;
     public class ProductsController : ApiController
     {
         private DataContext db = new DataContext();
@@ -20,14 +17,14 @@
         // GET: api/Products
         public IQueryable<Product> GetProducts()
         {
-            return db.Products;
+            return this.db.Products.OrderBy(p => p.Description);
         }
 
         // GET: api/Products/5
         [ResponseType(typeof(Product))]
         public async Task<IHttpActionResult> GetProduct(int id)
         {
-            Product product = await db.Products.FindAsync(id);
+            Product product = await this.db.Products.FindAsync(id);
             if (product == null)
             {
                 return NotFound();
@@ -50,11 +47,11 @@
                 return BadRequest();
             }
 
-            db.Entry(product).State = EntityState.Modified;
+            this.db.Entry(product).State = EntityState.Modified;
 
             try
             {
-                await db.SaveChangesAsync();
+                await this.db.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
             {
@@ -75,13 +72,17 @@
         [ResponseType(typeof(Product))]
         public async Task<IHttpActionResult> PostProduct(Product product)
         {
+            // Campos pendientes
+            product.IsAvailable = true;
+            product.PublishOn = DateTime.Now.ToUniversalTime();
+
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            db.Products.Add(product);
-            await db.SaveChangesAsync();
+            this.db.Products.Add(product);
+            await this.db.SaveChangesAsync();
 
             return CreatedAtRoute("DefaultApi", new { id = product.ProductID }, product);
         }
@@ -90,14 +91,14 @@
         [ResponseType(typeof(Product))]
         public async Task<IHttpActionResult> DeleteProduct(int id)
         {
-            Product product = await db.Products.FindAsync(id);
+            Product product = await this.db.Products.FindAsync(id);
             if (product == null)
             {
                 return NotFound();
             }
 
-            db.Products.Remove(product);
-            await db.SaveChangesAsync();
+            this.db.Products.Remove(product);
+            await this.db.SaveChangesAsync();
 
             return Ok(product);
         }
@@ -106,14 +107,14 @@
         {
             if (disposing)
             {
-                db.Dispose();
+                this.db.Dispose();
             }
             base.Dispose(disposing);
         }
 
         private bool ProductExists(int id)
         {
-            return db.Products.Count(e => e.ProductID == id) > 0;
+            return this.db.Products.Count(e => e.ProductID == id) > 0;
         }
     }
 }
