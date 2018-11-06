3e182e6a6f4b609c0ada3330ce4f9a15fb21c917
renriquehdez <renriquehdez@gmail.com>
2018-11-06 10:17:16 -0600

    Mapas Parte III - Final

--- Sales/Sales/Views/MapPage.xaml.cs
+++ Sales/Sales/Views/MapPage.xaml.cs
@@ -1,20 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-namespace Sales.Views
+﻿namespace Sales.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MapPage : ContentPage
-	{
-		public MapPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    using System;
+    using Plugin.Geolocator;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Maps;
+
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            this.Locator();
+        }
+
+        private async void Locator()
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 50;
+
+            var location = await locator.GetPositionAsync();
+            var position = new Position(location.Latitude, location.Longitude);
+            this.MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromKilometers(1)));
+        }
+
+        private void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            var zoomLevel = double.Parse(e.NewValue.ToString()) * 18.0;
+            var latlongdegrees = 360 / (Math.Pow(2, zoomLevel));
+            this.MyMap.MoveToRegion(new MapSpan(this.MyMap.VisibleRegion.Center, latlongdegrees, latlongdegrees));
+        }
+    }
 }
